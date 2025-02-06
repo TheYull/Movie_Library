d0dd39d15234fa53a32d0a1ea91eca3a32a8fe6a
Yuliia <the.smile.too@gmail.com>
2025-02-06 16:05:39 +0200

    add store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+import { store } from "./store/store";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
