99ba6ab1d251ea7013aedcf1ca1a084084a11b44
Yuliia <the.smile.too@gmail.com>
2025-02-06 15:28:16 +0200

    add: initial files

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <Provider>
+    <App />
+  </Provider>
+);
